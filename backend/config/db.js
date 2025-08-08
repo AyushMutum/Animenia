9dc9924ab9b7d9c13bddad71dbce0c86002b8d62
Ayush Mutum <ayushmutum6@gmail.com>
2025-08-08 15:32:06 +0530

    updated item.jsx

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,5 +4,8 @@ export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://ayushmutum:animenia@cluster0.0szi1vy.mongodb.net/Animeniaaa').then(() => {
         console.log("DB connected")
     })
+
 }
+
+
 // mongoose.connect("mongodb+srv://ayushmutum:animenia@cluster0.0szi1vy.mongodb.net/Animenia?retryWrites=true&w=majority&appName=Cluster0")
--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,6 +1,67 @@
 
 
 
+// import React, { useContext, useState } from "react";
+// import { ShopContext } from "../../Context/ShopContext";
+// import "./Item.css";
+// import star_icon from "../Assets/star_icon.png";
+// import { IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
+// import { Link } from "react-router-dom";
+
+// const Item = ({ id, name, price, description, image }) => {
+
+//   const { url, cartItems, addToCart, removeFromCart, } = useContext(ShopContext);
+
+//   // const[itemCount,setItemCount] = useState(0)
+
+//   const handleAddToCart = () => {
+//     addToCart(id);
+//   };
+
+//   const handleRemoveFromCart = () => {
+//     removeFromCart(id);
+//   };
+ 
+//   return (
+//     <div className="anime-item">
+//       <div className="anime-item-image-container">
+//         <Link to={`/product/${id}`}>
+//         <img onClick={window.scrollTo(0,0)} className="anime-item-image" src={`${url}/images/${image}`} alt={name} />
+        
+//         </Link>
+        
+//         {!cartItems[id] ? (
+//           <IoAddCircleOutline size={30} className="add" onClick={handleAddToCart} />
+//         ) : (
+//           <div className="anime-item-counter">
+//             <IoRemoveCircleOutline size={25} color="red"  className="counter-tag" onClick={handleRemoveFromCart} />
+//             <p>{cartItems[id]}</p>
+//             <IoAddCircleOutline size={25} color="green" className="counter-tag" onClick={handleAddToCart} />
+//           </div>
+//         )}
+//       </div>
+//       <div className="anime-item-info">
+//         <div className="anime-item-name-rating">
+//           <p>{name}</p>
+//           <img src={star_icon} alt="star" />
+//           <img src={star_icon} alt="star" />
+//           <img src={star_icon} alt="star" />
+//           <img src={star_icon} alt="star" />
+//         </div>
+//         {/* <p className="anime-item-desc">{description}</p> */}
+//         <p className="anime-item-price">₹{price}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Item;
+
+
+
+
+
+
 import React, { useContext, useState } from "react";
 import { ShopContext } from "../../Context/ShopContext";
 import "./Item.css";
@@ -57,49 +118,3 @@ const Item = ({ id, name, price, description, image }) => {
 
 export default Item;
 
-
-// import React, { useContext } from "react";
-// import { ShopContext } from "../../Context/ShopContext";
-// import "./Item.css";
-// import star_icon from "../Assets/star_icon.png";
-// import { IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
-
-// const Item = ({ id, name, price, description, image }) => {
-//   const { url, cartItems, addToCart, removeFromCart } = useContext(ShopContext);
-
-//   const handleAddToCart = () => {
-//     addToCart(id);
-//   };
-
-//   const handleRemoveFromCart = () => {
-//     removeFromCart(id);
-//   };
-
-//   return (
-//     <div className="anime-item">
-//       <div className="anime-item-image-container">
-//         <img className="anime-item-image" src={`${url}/images/${image}`} alt={name} />
-//         {!cartItems[id] ? (
-//           <IoAddCircleOutline size={30} className="add" onClick={handleAddToCart} />
-//         ) : (
-//           <div className="anime-item-counter">
-//             <IoRemoveCircleOutline size={25} color="red" className="counter-tag" onClick={handleRemoveFromCart} />
-//             <p>{cartItems[id]}</p>
-//             <IoAddCircleOutline size={25} color="green" className="counter-tag" onClick={handleAddToCart} />
-//           </div>
-//         )}
-//       </div>
-//       <div className="anime-item-info">
-//         <div className="anime-item-name-rating">
-//           <p>{name}</p>
-//           {[...Array(4)].map((_, index) => (
-//             <img key={index} src={star_icon} alt="star" />
-//           ))}
-//         </div>
-//         <p className="anime-item-price">₹{price}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Item;
